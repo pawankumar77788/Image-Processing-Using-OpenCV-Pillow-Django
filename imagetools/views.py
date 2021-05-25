774b66fe972f5b6acc4a1197e87c8f8f6318974b
pawankumar77788 <pawan1612314.com>
2021-05-25 22:28:53 +0530

    created superuser

--- imagetools/views.py
+++ imagetools/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+#from django.shortcuts import render  
+#importing loading from django template  
+from django.template import loader  
+# Create your views here.  
+from django.http import HttpResponse  
+def index(request):  
+   template = loader.get_template('index.html') # getting our template  
+   return HttpResponse(template.render())       # rendering the template in HttpResponse
