30c04c5a1f4ca2f3532c64f1704e47eb32d4f908
pawankumar77788 <pawan1612314.com>
2021-06-19 20:55:30 +0530

    Added live preview and resizeability, implemented the deepAi SRGAN API to Upscale

--- imagetools/views.py
+++ imagetools/views.py
@@ -3,10 +3,12 @@ from django.core import serializers
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import *
+import urllib.request
+import requests
 import json
 import cv2
 import time
-from cv2 import dnn_superres
+# from cv2 import dnn_superres
 from .forms import *
   
 # Create your views here.
@@ -25,28 +27,38 @@ def low_resolution_submit(request):
 def cropped_image(request):
     if request.is_ajax() and request.method == "POST":
         data_cordinates = json.loads(request.POST.get('data', ''))
+        resolution = json.loads(request.POST.get('resolution',''))
         data = request.FILES.get('image')
         form = originalresolution(original_resolution_Img = data)
         form.save()
         image = cv2.imread(originalresolution.objects.latest('original_resolution_Img').original_resolution_Img.path)
-        cropped_image = image[int(data_cordinates['x']):int(data_cordinates['x'])+150, int(data_cordinates['y']):int(data_cordinates['y'])+150]
+        cropped_image = image[int(data_cordinates['x']):int(data_cordinates['x'])+resolution['height'], int(data_cordinates['y']):int(data_cordinates['y'])+resolution['width']]
         cv2.imwrite("./media/images/lowresolution/cropped.jpg",cropped_image)
         form1 = lowresolution(low_resolution_Img = 'images/lowresolution/cropped.jpg')
         form1.save()
         lowresolution_img = cv2.imread(lowresolution.objects.latest('low_resolution_Img').low_resolution_Img.path)
-        path = "./models/EDSR_x4.pb"
-        # Create an SR object
-        sr = dnn_superres.DnnSuperResImpl_create()
-        sr.readModel(path)
-        sr.setModel("edsr", 4)
         start_time = time.time()
-        superresolution_img = sr.upsample(lowresolution_img)
-        cv2.imwrite("./media/images/superresolution/upscaled.jpg", superresolution_img)
+        r = requests.post(
+            "https://api.deepai.org/api/torch-srgan",
+            files={
+                'image': open("./media/images/lowresolution/cropped.jpg", 'rb'),
+            },
+            headers={'api-key': '05e3672f-71f5-412c-a0c4-75bcd67f075a'}
+        )
+        # path = "./models/EDSR_x4.pb"
+        # Create an SR object
+        # sr = dnn_superres.DnnSuperResImpl_create()
+        # sr.readModel(path)
+        # sr.setModel("edsr", 4)
+        # start_time = time.time()
+        # superresolution_img = sr.upsample(lowresolution_img)
+        # cv2.imwrite("./media/images/superresolution/upscaled.jpg", superresolution_img)
+        urllib.request.urlretrieve(r.json()['output_url'], "./media/images/superresolution/upscaled.jpg")
+        end_time = time.time()
+        time_taken = end_time - start_time
         form2 = superresolution(super_resolution_Img = 'images/superresolution/upscaled.jpg')
         form2.save()
         instance = serializers.serialize('json', superresolution.objects.all(),fields=('super_resolution_Img'))
-        end_time = time.time()
-        time_taken = end_time - start_time
         originalresolution.objects.all().delete()
         lowresolution.objects.all().delete()
         superresolution.objects.all().delete()
--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,38 @@
 let imgElement = document.getElementById('img_copy');
 let inputElement = document.getElementById('fileInput');
+var output = $("#contour").position();
 var x_cor, ycor;
 var img = document.querySelector('#img_element');
 var owi = document.getElementById('original_width');
 var ohi = document.getElementById('original_height');
 var mycan = document.getElementById('myCanvas');
+var mycancopy = document.getElementById('myCanvasCopy');
 var con = document.getElementById('contour');
+
+x_cor = (output.top - 8)*(img.naturalHeight/img.height)
+y_cor = (output.left - 8)*(img.naturalWidth/img.width)
+
 img.onload = function(){
+  $(function() { $("#contour").draggable({containment:"#img_element"}).resizable({containment:"#img_element"}); });
   document.getElementById('original_width').innerHTML = img.naturalWidth;
   document.getElementById('original_height').innerHTML = img.naturalHeight;
   document.getElementById('contour').style.width = String(mycan.width/(img.naturalWidth/img.width)) + "px";
   document.getElementById('contour').style.height = String(mycan.height/(img.naturalHeight/img.height)) + "px";
+  let src = cv.imread(imgElement);
+  let dst = new cv.Mat();
+  let rect = new cv.Rect(0,0,150,150);
+  dst = src.roi(rect);
+  cv.imshow('myCanvas', dst);
+  mycan.style.display = "none";
+  cv.imshow('myCanvasCopy', dst);
+  mycancopy.style.width = "250px";
+  mycancopy.style.height = "250px";
+  src.delete();
+  dst.delete();
   // let mat = cv.imread(imgElement);
   // cv.imshow('myCanvas', mat);
   // mat.delete();
 }
-$( function() {
-    $( "#contour" ).draggable({containment:"#img_element"});
-  } );
 var contour_element = document.getElementById('contour');
 contour_element.ondrag = function(){
   //console.log("moved");
@@ -34,11 +49,77 @@ contour_element.ondrag = function(){
   //             'image type: ' + src.type() + '\n');
   let dst = new cv.Mat();
   // You can try more different parameters
-  let rect = new cv.Rect((output.left - 8)*(img.naturalWidth/img.width), (output.top - 8)*(img.naturalHeight/img.height), mycan.width, mycan.height);
+  // let rect = new cv.Rect((output.left - 8)*(img.naturalWidth/img.width), (output.top - 8)*(img.naturalHeight/img.height), mycan.width, mycan.height);
+  let rect = new cv.Rect((output.left - 8)*(img.naturalWidth/img.width), (output.top - 8)*(img.naturalHeight/img.height), Math.ceil((parseInt(contour_element.style.width.slice(0,-2))/img.width)*img.naturalWidth), Math.ceil((parseInt(contour_element.style.height.slice(0,-2))/img.height)*img.naturalHeight));
   dst = src.roi(rect);
   cv.imshow('myCanvas', dst);
+  mycan.style.display = "none";
+  cv.imshow('myCanvasCopy', dst);
+  mycancopy.style.width = "250px";
+  mycancopy.style.height = "250px";
   src.delete();
   dst.delete();
+}
+
+$('#contour').bind('mousemove', function(e){
+    $('#tail').show();
+    if(e.pageY > 335){
+      if(e.pageX > 990){
+        $('#tail').css({
+           left:  e.pageX - 250,
+           top:   e.pageY - (e.pageY - 335)
+        });
+      }else{
+        $('#tail').css({
+           left:  e.pageX + 20,
+           top:   e.pageY - (e.pageY - 335)
+        });
+      }
+    }else{
+      if(e.pageX > 990){
+        $('#tail').css({
+           left:  e.pageX - 250,
+           top:   e.pageY
+        });
+      }else{
+        $('#tail').css({
+           left:  e.pageX + 20,
+           top:   e.pageY
+        });
+      }  
+    }
+    $('#contour').bind('mouseleave', function(e){
+    $('#tail').hide();
+});
+});
+
+contour_element.onresize = function(){
+  //console.log("moved");
+  // console.log(parseInt(contour_element.style.width.slice(0,-2)));
+  var output = $("#contour").position();
+  x_cor = (output.top - 8)*(img.naturalHeight/img.height)
+  y_cor = (output.left - 8)*(img.naturalWidth/img.width)
+  // document.getElementById('outputposition').innerHTML = String((output.top - 8)*(img.naturalHeight/500)) + " , " + String((output.left - 8)*(img.naturalWidth/1000));
+  let src = cv.imread(imgElement);
+  // console.log('image width: ' + src.cols + '\n' +
+  //             'image height: ' + src.rows + '\n' +
+  //             'image size: ' + src.size().width + '*' + src.size().height + '\n' +
+  //             'image depth: ' + src.depth() + '\n' +
+  //             'image channels ' + src.channels() + '\n' +
+  //             'image type: ' + src.type() + '\n');
+  let dst = new cv.Mat();
+  // You can try more different parameters
+  let rect = new cv.Rect((output.left - 8)*(img.naturalWidth/img.width), (output.top - 8)*(img.naturalHeight/img.height),  Math.ceil((parseInt(contour_element.style.width.slice(0,-2))/img.width)*img.naturalWidth), Math.ceil((parseInt(contour_element.style.height.slice(0,-2))/img.height)*img.naturalHeight));
+  dst = src.roi(rect);
+  // console.log(dst,src)
+  cv.imshow('myCanvas', dst);
+  mycan.style.display = "none";
+  cv.imshow('myCanvasCopy', dst);
+  mycancopy.style.width = "250px";
+  mycancopy.style.height = "250px";
+  src.delete();
+  dst.delete();
+  document.getElementById('res').innerHTML = String(Math.ceil((parseInt(contour_element.style.width.slice(0,-2))/img.width)*img.naturalWidth)) + " X " + String(Math.ceil((parseInt(contour_element.style.height.slice(0,-2))/img.height)*img.naturalHeight));
 }
 
 inputElement.addEventListener('change', (e) => {
@@ -60,8 +141,8 @@ inputElement.addEventListener('change', (e) => {
 function function2(){
   $('#result_img').css("display","none");
   $('#compare_img').css("display","none");
-  document.getElementById("result_img").width = "600";
-  document.getElementById("result_img").height = "600";
+  document.getElementById("result_img").width = document.getElementById("result_img").naturalWidth;
+  document.getElementById("result_img").height = document.getElementById("result_img").naturalHeight;
   var downloadimage_file = document.getElementById('result_img');
   var canvas2 = document.getElementById('downloadfile');
   var ctx2 = canvas2.getContext('2d');
@@ -120,6 +201,7 @@ function function1(){
   var formData = new FormData();  
   formData.append('image',$('#fileInput')[0].files[0])
   formData.append('data',JSON.stringify({'x': x_cor,'y': y_cor}))
+  formData.append('resolution',JSON.stringify({'width': Math.ceil((parseInt(contour_element.style.width.slice(0,-2))/img.width)*img.naturalWidth),'height':Math.ceil((parseInt(contour_element.style.height.slice(0,-2))/img.height)*img.naturalHeight)}))
   $.ajax({
     type: 'POST',
     url: 'cropped_image',
@@ -137,9 +219,9 @@ function function1(){
       complete.scrollIntoView({behavior: "smooth"})
     },
     complete:function(data){
-      // Hide image container
-      $("#ajax_status").text("Successfully rendered in "+String(data.responseJSON["time_taken"])+" Seconds.");
-      var instance = JSON.parse(data.responseJSON["instance"]);
+      // Hide image container_taken
+      $("#ajax_status").text("Successfully rendered in "+String(JSON.parse(data.responseText).time_taken)+" Seconds.");
+      var instance = JSON.parse(JSON.parse(data.responseText).instance);
       $('#result_img').attr("src","http://127.0.0.1:8000/media/"+instance[0].fields.super_resolution_Img);
       $('#result_img').css("display","inline-block");
       $('#compare_img').attr("src","http://127.0.0.1:8000/media/images/lowresolution/cropped.jpg");
